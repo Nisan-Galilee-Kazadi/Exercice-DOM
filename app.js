6a8bc8c7acc65d88fdc788971871a6c003cf27fb
Nisan-Galilee-Kazadi <galileokazadi45@gmail.com>
2024-12-28 14:09:35 +0100

    first commit

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
-const response = await fetch(data.json);
+let articles;
+async function fetchData() {
+  const response = await fetch("/data.json");
 
-const pieces = await response.json();
+  const pieces = await response.json();
+  articles = pieces;
+  console.log(pieces);
+}
 
-console.log(pieces);
+fetchData().then(() => {
+  const nomPiece = document.createElement("h5");
+  nomPiece.innerText = articles[0].nom;
+  const prixPiece = document.createElement("h6");
+  prixPiece.innerText = articles[0].prix;
+  const categoriePiece = document.createElement("h6");
+  categoriePiece.innerText = articles[0].categorie;
+  const imagePiece = document.createElement("img");
+  imagePiece.src = articles[0].image;
+
+  const enStock = document.createElement("p");
+  if (articles[0].enStock == true) {
+    enStock.innerText = "En Stock";
+  } else {
+    enStock.innerText = "Pas en stock";
+  }
+
+  const piece = document.querySelector(".piece");
+
+  piece.appendChild(nomPiece);
+  piece.appendChild(prixPiece);
+  piece.appendChild(categoriePiece);
+  piece.appendChild(imagePiece);
+  piece.appendChild(enStock);
+});
+
+// nomPiece.innerText = articles[0].nom;
